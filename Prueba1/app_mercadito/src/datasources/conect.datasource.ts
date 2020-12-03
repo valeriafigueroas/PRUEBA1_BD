5318503d689c0aed9ccab152a035d733074e652d
valeriafigueroas <73376019+valeriafigueroas@users.noreply.github.com>
2020-12-03 17:32:56 -0600

    dd

--- Prueba1/app_mercadito/src/datasources/conect.datasource.ts
+++ Prueba1/app_mercadito/src/datasources/conect.datasource.ts
@@ -4,12 +4,12 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'conect',
   connector: 'mssql',
-  url: 'mssql://sa:Spshouse123.@DELL7040/Mercadito',
-  host: 'DELL7040',
+  url: 'mssql://valeriafigueroas_SQLLogin_1:3lr9b6gmma@Mercadito618111272.mssql.somee.com/Mercadito618111272',
+  host: 'Mercadito618111272.mssql.somee.com',
   port: 1433,
-  user: 'sa',
-  password: 'Spshouse123.',
-  database: 'Mercadito'
+  user: 'valeriafigueroas_SQLLogin_1',
+  password: '3lr9b6gmma',
+  database: 'Mercadito618111272'
 };
 
 // Observe application's life cycle to disconnect the datasource when
